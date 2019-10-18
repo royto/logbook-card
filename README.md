c248044731b49875aafc5aca883f4a1bc800c059
Julien Roy <se302242@se.com>
2019-10-18 13:42:07 +0200

    First working version
    update screenshot

---
+++ README.md
@@ -0,0 +1,31 @@
+# Logbook card
+
+A custom [Lovelace](https://www.home-assistant.io/lovelace/) component for displaying history of an entity for [Home Assistant](https://github.com/home-assistant/home-assistant).
+
+![logbook card example](images/screenshot.png)
+
+## Using the card
+
+### Options
+
+#### Card options
+
+| Name | Type | Default | Since | Default | Description |
+|------|------|---------|-------|---------|-------------|
+| type | string | **required** | v0.1 | | `custom:logbook-card`|
+| entity | string | **required** | v0.1 | | An entity_id.|
+| title | string | optional | v0.1 | *friendly_name* History | Card title|
+| history | integer | optional | v0.1 | 5 | Numbers of days of history of the logbook |
+| hiddenState | string[] | optional | v0.1 | [] | States to hide|
+| desc | bool | optional | v0.1 | True | is logbook ordered descending|
+
+### Example usage
+
+```yaml
+type: 'custom:history-card'
+desc: true
+entity: sun.sun
+hiddenState:
+  - above_horizon
+title: Day history
+```
---
+++ logbook-card.js
@@ -0,0 +1,141 @@
+class LogbookCard extends Polymer.Element {
+
+    static get template() {
+        return Polymer.html`
+          <style>
+            .title {
+              font-size: 20px;
+              padding: 16px;
+              text-align: center;
+              white-space: nowrap;
+              text-overflow: ellipsis;
+              overflow: hidden;
+            }
+            .content {
+              cursor: pointer;
+            }
+            .item {
+              padding: 5px;
+              font-size: 1.2em;
+            }
+            .duration {
+              font-size: 0.85em;
+              font-style: italic;
+              float: right;
+            }
+
+            .date {
+              font-size: 0.7em;
+            }
+          </style>
+          <ha-card hass="[[_hass]]" config="[[_config]]">
+            <template is="dom-if" if="{{title}}">
+              <div class="title" style="[[contentText]]">[[title]]</div>
+            </template>
+            <div class="content grid" style="[[contentStyle]]">
+              <div>
+                <template is="dom-repeat" items="{{history}}">
+                  <div class="item">
+                    <span>[[item.state]]</span> 
+                    <span class="duration">[[getDuration(item.duration)]]</span>
+                    <div class="date"> [[_displayDate(item.start)]] - [[_displayDate(item.end)]]</div>
+                  </div>
+                </template>
+              </div>
+            </div>
+            </ha-card>
+        `;
+    }
+
+    getCardSize() {
+        if (this.title && this.showButtons) return 5;
+        if (this.title || this.showButtons) return 4;
+        return 3;
+    }
+
+    _displayDate(date) {
+        return date.toLocaleString(this._hass.language);
+    }
+
+    getDuration(durationInMs) {
+        if (!durationInMs) {
+            return '';
+        }
+        const durationInS = durationInMs / 1000;
+        if (durationInS < 60) {
+            return Math.round(durationInS) + 's';
+        }
+        const durationInMin = durationInS / 60;
+        if (durationInMin < 60) {
+            return Math.round(durationInMin) + 'm';
+        }
+        const durationInHours = durationInMin / 60;
+        if (durationInHours < 24) {
+            return Math.round(durationInHours) + 'h';
+        }
+        return Math.round(durationInHours / 24) + 'd';
+    }
+
+    setConfig(config) {
+        const labels = {
+            duration: 'duration'
+        };
+
+        const defaultConfig = {
+            history: 5,
+            hiddenState: [],
+            desc: true
+        };
+
+        this._config = Object.assign(defaultConfig, config);
+
+        if (!config.entity) throw new Error('Please define an entity.');
+        //if hiddenState != Array 
+    }
+
+    set hass(hass) {
+        this._hass = hass;
+
+        if (hass && this._config) {
+            this.stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
+
+            const startDate = new Date(new Date().setDate(new Date().getDate() - this._config.history));
+
+            const uri = 'history/period/' + startDate.toISOString() +
+                '?filter_entity_id=' + this._config.entity +
+                '&end_time=' + new Date().toISOString();
+
+            this._hass.callApi('get', uri)
+                .then(history => {
+                    this.history = history[0].map(hist => ({
+                        state: hist.state,
+                        start: new Date(hist.last_changed)
+                    }))
+                        .map((x, i, arr) => {
+                            if (i < arr.length - 1) {
+                                return {
+                                    ...x,
+                                    end: arr[i + 1].start
+                                }
+                            };
+                            return { ...x, end: new Date() }
+                        })
+                        .map(x => ({
+                            ...x,
+                            duration: x.end - x.start
+                        }))
+                        .filter(x => !this._config.hiddenState.includes(x.state));
+
+                    if (this._config.desc === true) {
+                        this.history = this.history.reverse();
+                    }
+                });
+
+            if (this.stateObj) {
+                this.title = this._config.title !== false && (this._config.title || this.stateObj.attributes.friendly_name + ' History');
+            }
+        }
+    }
+}
+
+customElements.define('logbook-card', LogbookCard);
