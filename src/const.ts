db0a651d54788c4d9b90b1c6f828a5c2bd8d6ae7
royto <royto81+github@gmail.com>
2023-10-27 22:21:27 +0200

    bump to 1.13.1

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 import { ShowConfiguration, SeparatorStyleConfig, DurationConfig } from './types';
-export const CARD_VERSION = '1.13.0';
+export const CARD_VERSION = '1.13.1';
 
 export const DEFAULT_SHOW: ShowConfiguration = {
   state: true,
